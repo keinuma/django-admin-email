3aa275ae2da3f45677cd755e4a17249b8e929445
keinuma <nununu.mono@gmail.com>
2018-11-18 23:55:17 +0900

    Add gmail settings

--- config/settings.py
+++ config/settings.py
@@ -118,3 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Email Setting
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = '587'
